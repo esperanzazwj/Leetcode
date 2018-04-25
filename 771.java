ba956957544c5f85f23e165a5e52d707fe94a704
esperanzazwj <624585443@qq.com>
2018-04-25 22:58:52 +0800

    ==

---
+++ 771.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int numJewelsInStones(String J, String S) {
+        Set<Character> set = new HashSet<>();
+        int cnt=0;
+        for(int i=0;i<J.length();i++)
+            set.add(J.charAt(i));
+        for(int i=0;i<S.length();i++)
+            if(set.contains(S.charAt(i))) cnt++;
+        return cnt;
+    }
+}
