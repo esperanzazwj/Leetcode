370b5370a43b6b154d2c76354d8ab98dd03e56dc
esperanzazwj <624585443@qq.com>
2018-04-27 15:05:06 +0800

    待完善

---
+++ 535.java
@@ -0,0 +1,21 @@
+public class Codec {
+    
+    Map<Integer,String> map = new HashMap<>();
+    int i=1;
+    // Encodes a URL to a shortened URL.
+    public String encode(String longUrl) {
+        map.put(i,longUrl);
+        return "http://tinyurl.com/"+i++;
+        
+    }
+
+    // Decodes a shortened URL to its original URL.
+    public String decode(String shortUrl) {
+        return map.get(Integer.parseInt(shortUrl.substring(19)));
+        
+    }
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec = new Codec();
+// codec.decode(codec.encode(url));
